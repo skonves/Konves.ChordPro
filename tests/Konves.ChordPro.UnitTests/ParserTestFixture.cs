e633ee9276d0578487c106b3be6399112f1e46b2
Steve Konves <stephen.konves@gmail.com>
2016-10-04 21:40:27 -0700

    Remove dead code

--- tests/Konves.ChordPro.UnitTests/ParserTestFixture.cs
+++ tests/Konves.ChordPro.UnitTests/ParserTestFixture.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Konves.ChordPro.Directives;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- tests/Konves.ChordPro.UnitTests/UnitTest1.cs
+++ tests/Konves.ChordPro.UnitTests/UnitTest1.cs
@@ -2,8 +2,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Reflection;
 using System.IO;
-using Antlr4.Runtime;
-using static Konves.ChordPro.ChordProParser;
 using System.Text;
 using System.Collections.Generic;
 using System.Linq;
@@ -29,33 +27,6 @@ namespace Konves.ChordPro.UnitTests
 				// Act
 				result = sut.Parse().ToList();
 			}
-
-			// Assert
-			Assert.IsNotNull(result);
-		}
-
-		[TestMethod]
-		public void TestMethod1()
-		{
-			// Arrange
-			var assembly = Assembly.GetExecutingAssembly();
-			var resourceName = "Konves.ChordPro.UnitTests.Data.love-me-tender.cho";
-
-			AntlrInputStream inputStream;
-
-			using (Stream stream = assembly.GetManifestResourceStream(resourceName))
-			{
-				inputStream = new AntlrInputStream(stream);
-			}
-
-			ChordProLexer lexer = new ChordProLexer(inputStream);
-			CommonTokenStream commonTokenStream = new CommonTokenStream(lexer);
-			ChordProParser parser = new ChordProParser(commonTokenStream);
-
-			parser.RemoveErrorListeners();
-
-			// Act
-			DocumentContext result = parser.document();
 
 			// Assert
 			Assert.IsNotNull(result);
